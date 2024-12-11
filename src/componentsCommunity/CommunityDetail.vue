08645f888df4a6c138a6f45f6a6b605e4e7765a9
weishenying <1724579910@>
2024-12-11 15:08:14 +0800

    删除

--- src/componentsCommunity/CommunityDetail.vue
+++ src/componentsCommunity/CommunityDetail.vue
@@ -5,12 +5,6 @@
 export default {
 data(){
 return{
-    buttons: [
-        { text: "职场与内推", icon: "el-icon-suitcase" },
-        { text: "技术交流", icon: "el-icon-chat-dot-round" },
-        { text: "创社区", icon: "el-icon-share" },
-        { text: "意见反馈", icon: "el-icon-message" },
-    ],
 };
 },
 
